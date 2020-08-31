67486c09a1bde399fb3848317d92a610697efe19
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-31 15:31:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,14 +3,12 @@ def join_nested_strings(src)
   combine_string = []
   while row_index < src.count do
     element_index = 0
-    element_string = ""
     while element_index < src[row_index].count do
-      if src[row_index][element_index] == String
-        element_string += src[row_index][element_string]
+      if src[row_index][element_index][0] == String
+        combine_string << src[row_index][element_string]
       end
     element_index += 1
     end
-  combine_string << element_string
   row_index += 1
   end  
 combine_string.join
